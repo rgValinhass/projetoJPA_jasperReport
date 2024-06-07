44ccdf7b3dda44cbe4c9b9ce0f1b00d56a3591de
rgValinhass <116306770+rgValinhass@users.noreply.github.com>
2024-06-07 16:11:34 -0300

    Add files via upload

---
+++ src/controller/daoTecnico.java
@@ -0,0 +1,25 @@
+
+package controller;
+
+import abstratas.Dados;
+import abstratas.dao;
+import java.util.List;
+import javax.persistence.Query;
+import model.Tecnico;
+
+
+public class daoTecnico extends dao<Tecnico>{
+    public List<Tecnico> read(){
+        String JPQL = "select t from Tecnico t order by t.nome";
+        Query query = Dados.getManager().createQuery(JPQL);
+        return query.getResultList();
+    }
+    
+    public List<Tecnico> read(String filtroNome){
+        String JPQL = "select t from Tecnico t where t.nome like ?1 order by t.nome";
+        Query query = Dados.getManager().createQuery(JPQL);
+        query.setParameter(1, "%"+filtroNome.toUpperCase()+"%");
+        return query.getResultList();
+    }
+    
+}
